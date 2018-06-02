975c660aafb34c373a50bfe3d4a2a7f5cedd7c1e
ryosaitok <ryo.saito.k@gmail.com>
2018-06-02 16:22:47 +0900

    クオーテーションマークのエラー解消

--- server/index.js
+++ server/index.js
@@ -21,4 +21,4 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/../dist/index.html'));
 });
 
-console.log(`Server listening on ${port}`);
+console.log('Server listening on ${port}');
